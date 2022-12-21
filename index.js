e5d9d8f8fff1e39404bb1c1301487167f70cf4d8
kipyek <deniskipyegon88@gmail.com>
2022-12-21 12:57:20 +0300

    Footer

---
+++ index.js
@@ -0,0 +1,19 @@
+document.getElementById("myFooter").innerHTML =
+  "<div id='footercontent'>"
+  + "<h1>Deliverance Church</h1>"
+  + "<p>In this footer section HTML CSS I have added five social media icons. The following HTML and CSS codes have</p>"
+  + "<p>been used to create these social icons.</p >"
+  + "<ul id='socials'>"
+  + "<li><a href='https://www.facebook.com/DCKaplong/'><i class='fa fa-facebook'></i></a></li>"
+  + "<li><a href='#''><i class='fa fa-twitter'></i></a></li>"
+  + "<li><a href='https://www.youtube.com/@deliverancechurchkaplong527'><i class='fa fa-youtube'></i></a></li>"
+  + "</ul >"
+  + "<div id='footerbottom'>"
+  + "<p id='para'>Copyright &copy; " + new Date().getFullYear() + " DCK. All"
+  + " rights reserved.</p>"
+  + "</div>"
+
+
+
+
+
